97e842e116a2487963e83ae1b990b16af9e83313
Kenneth <kennethrdz@hotmail.com>
2023-03-11 10:28:21 -0600

    Data adquisition complete

---
+++ linear-regression/notas.md
@@ -0,0 +1,3 @@
+# Regresión lineal
+
+La más común es utilizar el método de mínimos cuadrados. Se puede utilizar un programa propio o utilizar algunas librería. Así se puede obtener una recta para graficar los resultados
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import pandas as pd
 
 def data_adquisition(dataset: str): 
-    df_vgsales = pd.
-    pass
+    df = pd.read_csv(dataset+".csv")
+    print("Raw")
+    print(df)
+    df = df.drop(labels="Rank", axis="columns")
+    df = df.dropna(axis="index")
+    df.to_csv(dataset+"_clean.csv")
 
 
-if __name_=="__main__":
-    dataset = "vgsales.csv"
-    pass
+if __name__=="__main__":
+    dataset = "vgsales"
+    data_adquisition(dataset)
