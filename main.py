91d6edb597ecad78d659ca3c92265490270a4142
Kenneth <kennethrdz@hotmail.com>
2023-03-11 10:33:09 -0600

    Start data analysis

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ def data_adquisition(dataset: str):
     print(df)
     df = df.drop(labels="Rank", axis="columns")
     df = df.dropna(axis="index")
-    df.to_csv(dataset+"_clean.csv")
+    df.to_csv(dataset+"_clean.csv", index=False)
 
+def data_analysis(dataset: str):
+    df = pd.read_csv(dataset+"_clean.csv")
+    print("Clean")
+    print(df)
+    pass
 
 if __name__=="__main__":
     dataset = "vgsales"
     data_adquisition(dataset)
+    data_analysis(dataset)
